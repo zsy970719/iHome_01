c96a95e717f8a3235268c8075865c7552090769a
zsy970719 <zsy970719@163.com>
2018-04-19 19:16:40 +0800

    第一个版本：第三天：1，添加图片验证码过期时间和删除上次验证码

--- iHome/api_1_0/verify.py
+++ iHome/api_1_0/verify.py
@@ -8,12 +8,17 @@ from iHome import redis_store
 from . import api
 from iHome.until.captcha.captcha import captcha
 from  iHome.until.response_code import RET
+from iHome import constants
 
+
+#定义变量记录上一次的uuid
+last_uuid = ''
 
 
 @api.route('/image_code',methods=['GET'])
 def get_image_code():
     """提供图片验证码"""
+
     #1，获取uuid，并效验uuid
     uuid = request.args.get('uuid')
     if not uuid:
@@ -22,15 +27,22 @@ def get_image_code():
     name, text, image = captcha.generate_captcha()
     #3，使用redis数据库缓存图片验证码，uuid作为key
     try:
-        redis_store.set('ImageCode:%s'%uuid,text)
+        if last_uuid:
+            redis_store.delete('ImageCode:%s'%last_uuid)
+
+        #过期时间为300秒
+        redis_store.set('ImageCode:%s'%uuid,text,constants.IMAGE_CODE_REDIS_EXPIRES)
+        # redis_store.delete('key:last_uuid')
     except Exception as  e:
         print e
         return jsonify(errno=RET.DBERR,errmsg='保存验证码失败')
 
-    #修改响应头信息，指定响应的内容是image/jpg
+    #记录当前的uuid
+    global last_uuid
+    last_uuid = uuid
+
+    #4，修改响应头信息，指定响应的内容是image/jpg
     response = make_response(image)
     response.headers['Content-Type']='image/jpg'
-
-
     #响应图片验证码
     return response
