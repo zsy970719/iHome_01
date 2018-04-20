6972093b6f203120de5da32c728f6e6fac055dac
zsy970719 <zsy970719@163.com>
2018-04-20 14:31:05 +0800

    第一个版本：第三天：6,实现csrf_token的效验逻辑

--- iHome/web_html.py
+++ iHome/web_html.py
@@ -2,6 +2,9 @@
 from flask import Blueprint,current_app
 
 #创建静态html文件蓝图
+from flask import make_response
+from flask_wtf.csrf import generate_csrf
+
 html_blue = Blueprint('html',__name__)
 
 
@@ -20,6 +23,16 @@ def get_static_html(file_name):
     if file_name != 'favicon.ico':
         file_name = 'html/%s' % file_name
 
-    #根据file_name拼接的全路径，去项目路径中查找静态html文件，并响应给浏览器
-    return current_app.send_static_file(file_name)
 
+    #创建响应对象
+    response = make_response(current_app.send_static_file(file_name))
+    #生成csrf_token
+    csrf_token = generate_csrf()
+    #将csrf_token写入到cookie
+    response.set_cookie('csrf_token',csrf_token)
+
+
+
+    #根据file_name拼接的全路径，去项目路径中查找静态html文件，并响应给浏览器
+    # return current_app.send_static_file(file_name)
+    return response
